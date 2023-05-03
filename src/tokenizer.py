fe6a1c31ea862d1efc92dd6d6aed6218ff16e498
Jon Burnworth <titaniumultd@gmail.com>
2023-05-03 14:57:50 -0700

    added ngrams, token length and custom f() filter

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -2,11 +2,14 @@ import re
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 
 class Tokenizer:
-    def __init__(self, lowercase=False, delimiters=None, remove_stopwords=False, stem=False, lemmatize=False):
+    def __init__(self, lowercase=False, delimiters=None, remove_stopwords=False, stem=False, lemmatize=False, ngram=1, min_token_length=1, custom_filter=None):
         self.lowercase = lowercase
         self.remove_stopwords = remove_stopwords
         self.stem = stem
         self.lemmatize = lemmatize
+        self.ngram = ngram
+        self.min_token_length = min_token_length
+        self.custom_filter = custom_filter
         self.stopwords_list = [
             "a", "an", "and", "are", "as", "at", "be", "by", "for", "from", "has", "he", "in", "is", "it",
             "its", "of", "on", "that", "the", "to", "was", "were", "will", "with"
@@ -32,6 +35,13 @@ class Tokenizer:
             text = text.replace(contraction, expansion)
         return text
 
+    def generate_ngrams(self, tokens, n):
+        ngrams = []
+        for i in range(len(tokens) - n + 1):
+            ngram = ' '.join(tokens[i:i+n])
+            ngrams.append(ngram)
+        return ngrams
+
     def tokenize(self, text):
         text = self.expand_contractions(text)
         tokens = re.findall(self.pattern, text)
@@ -47,5 +57,16 @@ class Tokenizer:
 
         if self.lemmatize:
             tokens = [self.lemmatizer.lemmatize(token) for token in tokens]
+
+        if self.ngram > 1:
+            tokens = self.generate_ngrams(tokens, self.ngram)
+
+        if self.custom_filter is not None:
+            tokens = [token for token in tokens if self.custom_filter(token)]
+
+        
+        tokens = [token for token in tokens if len(token) >= self.min_token_length]
+
+
 
         return tokens
